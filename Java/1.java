b623af5af1cdcf1cf4fbdddde553aa0a14d3b6b3
Krishaay <krishaayjois@gmail.com>
2024-10-06 10:05:54 +0530

    Update 1.java

--- Java/1.java
+++ Java/1.java
@@ -7,7 +7,7 @@
 // Example 1:
 // Input: nums = [1,2,10,5,7]
 // Output: true
-// Explanation: By removing 10 at index 2 from nums, it becomes [1,2,5,7].
+// Explanation: By removing 10 at index 2 from nums, it becomes [1,2,5,7].dasdasdasdasdasdasd
 // [1,2,5,7] is strictly increasing, so return true.
 
 // Example 2:
