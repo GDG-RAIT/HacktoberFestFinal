9fc0f227e563347a6bfe6af7d195f5fd8b95d858
SanchitGade <sanchitgade06@gmail.com>
2024-10-06 14:47:33 +0530

    Question - 11.c
    C programming

--- C/11.c
+++ C/11.c
@@ -32,6 +32,57 @@
 // 1 <= password.length <= 50
 // password consists of letters, digits, dot '.' or exclamation mark '!'.
 
-int strongPasswordChecker(char* password) {
-    
+int strongPasswordChecker(char* password) 
+{
+    int len = strlen(password);
+    int lowercase = 0, uppercase = 0, digit = 0;
+    int missing = 3;
+    int repeat = 0;
+
+    if (len < 6)
+    {
+        missing = missing + (6 - len);
+    }
+    else if (len > 20) 
+    {
+        missing = missing +  (len - 20);
+    }
+
+    for (int i = 0; i < len; i++)
+    {
+        if (islower(password[i]))
+        {
+            lowercase = 1;
+        } else if (isupper(password[i])) 
+        {
+            uppercase = 1;
+        } else if (isdigit(password[i])) 
+        {
+            digit = 1;
+        }
+    }
+    missing = missing -  (lowercase + uppercase + digit);
+
+    for (int i = 0; i < len - 2; i++)
+    {
+        if (password[i] == password[i + 1] && password[i] == password[i + 2])
+        {
+            repeat++;
+            i += 2; 
+        }
+    }
+
+    int steps = missing;
+    if (repeat > 0)
+    {
+        steps = (steps > repeat) ? steps : repeat;
+    }
+    return steps;
+}
+
+int main() {
+    char password[] = "aA1";
+    int steps = passCheck(password);
+    printf(" STEPS NEEDED: %d\n", steps);
+    return 0;
 }
