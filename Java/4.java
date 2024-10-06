bf7b9ffdd1d5f3e8f0b0e63acfe17e0db86c4d4d
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2024-10-06 13:46:11 +0530

    java 4 hard

--- Java/4.java
+++ Java/4.java
@@ -13,19 +13,39 @@
 // Output: [1]
  
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        
+// Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+
+class sol {
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode s = new ListNode();
+        s.next = head;
+        ListNode s1 = s;
+        ListNode s2 = s;
+    
+
+        for(int i = 0; i < n ; i++)
+        {
+            s1 = s1.next;
+        }
+    
+        for(int i = 0; s1.next != null; i++)
+        {
+            s1 = s1.next;
+            s2 = s2.next;
+        }
+        s = null;
+        s2.next.next = null;
+        return head;
     }
 }
+
