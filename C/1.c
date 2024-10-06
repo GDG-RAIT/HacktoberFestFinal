fe7413df344d04c4d25c6241a5b07f7a4144fe57
shreyas <85345896+ShreyasAndurlekar@users.noreply.github.com>
2024-10-06 11:36:37 +0530

    1.c

--- C/1.c
+++ C/1.c
@@ -26,8 +26,39 @@
 // Explanation: The result of removing any element is [1,1].
 // [1,1] is not strictly increasing, so return false.
  
+#include <stdio.h>
+#include <stdbool.h>
 
 bool canBeIncreasing(int* nums, int numsSize) {
+    int count = 0;  // Count the number of elements we need to remove
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] <= nums[i - 1]) {
+            count++;
+            // If more than one element needs to be removed, return false
+            if (count > 1) 
+               return false;
     
+            // Check if we can remove nums[i] or nums[i - 1]
+            if (i > 1 && nums[i] <= nums[i - 2] && (i + 1 >= numsSize || nums[i + 1] <= nums[i - 1])) {
+                // If removing nums[i] doesn't work, we must remove nums[i - 1]
+                nums[i - 1] = nums[i];  // "Remove" nums[i - 1] by replacing it
+            }
+        }
+    
+}
+    
+    // If we reach here, we can remove at most one element
+    if(count <= 1)
+        printf("True");
+    else
+        printf("False");
+        
+    return count <= 1;
+}
+
+int main(){
+    int test[] = {1,2,10,5,7};
+    
+    canBeIncreasing(test, 5);
 }
 
