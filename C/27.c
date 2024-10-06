0d16ece4c5f45334e27edc256670f0a5439e6723
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-06 14:13:35 +0530

    triangle

--- C/27.c
+++ C/27.c
@@ -31,6 +31,30 @@
 // nums.length == 3
 // 1 <= nums[i] <= 100
 
+#include <stdio.h>
+#include <string.h>
+
 char* triangleType(int* nums, int numsSize) {
-    
+  
+    int a = nums[0], b = nums[1], c = nums[2];
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        return "none";
+    }
+
+    if (a == b && b == c) {
+        return "equilateral";
+    } else if (a == b || b == c || a == c) {
+        return "isosceles";
+    } else {
+        return "scalene";
+    }
+}
+
+int main(){
+
+    int test[] = {3,4,5};
+    printf("%s",triangleType(test,3));
+
+    return 0;
 }
