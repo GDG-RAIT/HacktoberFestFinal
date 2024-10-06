96534e328458002af43bbf9cf7cad9146fbf6e19
Varun Raj <140655393+itsnotcloud@users.noreply.github.com>
2024-10-06 13:32:01 +0530

    Update 27.java
    JAVA 27

--- Java/27.java
+++ Java/27.java
@@ -32,7 +32,27 @@
 // 1 <= nums[i] <= 100
 
 class Solution {
+<<<<<<< Updated upstream
     public String triangleType(int[] nums) {
         
+=======
+    public static String triangleType(int[] nums) {
+        String answer= new String();
+        if(nums[0]==nums[1]&&nums[1]==nums[2]&&nums[0]==nums[2]){
+            answer="Equilateral";
+        }
+        else if(nums[0]+nums[1]>nums[2]&&nums[1]+nums[2]>nums[0]&&nums[0]+nums[2]>nums[1]){
+            answer="Scalene";
+        }
+        else if(nums[0]==nums[1]||nums[1]==nums[2]||nums[0]==nums[2]){
+            answer="isosceles";
+        }
+    return answer;
+}
+    public static void main(String[] args){
+        int[] nums= {3,4,5};
+        String output=triangleType(nums);
+        System.out.println(output);
+>>>>>>> Stashed changes
     }
 }
