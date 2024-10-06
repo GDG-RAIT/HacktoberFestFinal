eb2bbe06fce51e108189d73d633479430fb159f7
Krishaay <krishaayjois@gmail.com>
2024-10-06 09:22:32 +0530

    Update 1.c

--- C/1.c
+++ C/1.c
@@ -28,6 +28,13 @@
  
 
 bool canBeIncreasing(int* nums, int numsSize) {
-    
+    int count = 0;
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] <= nums[i - 1]) {
+            count++;
+            if (count > 1) return 0;
+            if (i > 1 && nums[i] <= nums[i - 2] && nums[i + 1] <= nums[i - 1]) return 0;
+        }
+    }
+    return 1;
 }
-
